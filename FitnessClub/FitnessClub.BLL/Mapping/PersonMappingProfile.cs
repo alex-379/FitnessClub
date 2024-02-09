2be36d512fdf50c50febd8f96bdb1da8afcdd80b
alex-379 <alexey_379@mail.ru>
2024-02-09 20:43:29 +1000

    create bll

---
+++ FitnessClub/FitnessClub.BLL/Mapping/PersonMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FitnessClub.BLL.Models.PersonModels.InputModels;
+using FitnessClub.BLL.Models.PersonModels.OutputModels;
+using FitnessClub.DAL.Dtos;
+
+namespace FitnessClub.BLL.Mapping
+{
+    public class PersonMappingProfile:Profile
+    {
+        public PersonMappingProfile()
+        {
+            CreateMap<PersonDto, PersonOutputModel>();
+
+            CreateMap<PersonInputModel, PersonDto>();
+        }
+    }
+}
---
+++ FitnessClub/FitnessClub.BLL/Models/PersonModels/InputModels/PersonInputModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.BLL.Models.PersonModels.InputModels
+{
+    public class PersonInputModel
+    {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string? FamilyName { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? DateBirth { get; set; }
+
+        public bool? Sex { get; set; }
+    }
+}
