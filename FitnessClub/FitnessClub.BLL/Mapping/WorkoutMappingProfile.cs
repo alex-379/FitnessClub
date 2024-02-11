10d99b69c4f5f9f376d579632e87fabe5305b3f5
Dmitriy Dmitriy <dbilica19@gmail.com>
2024-02-11 15:17:16 +0300

    добавил наследование

--- FitnessClub/FitnessClub.BLL/Mapping/WorkoutMappingProfile.cs
+++ FitnessClub/FitnessClub.BLL/Mapping/WorkoutMappingProfile.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using AutoMapper;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FitnessClub.BLL.Mapping
 {
-    public class WorkoutMappingProfile
+    public class WorkoutMappingProfile: Profile
     {
+
     }
 }
