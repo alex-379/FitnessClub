33ada1570cf1e48b3b6be3a44b498c5f9303ce92
Василий Мысляев <myslyaev@gmail.com>
2024-02-14 22:50:54 +0300

    Add GymMappingProfile
    Add GymMappingProfile
    Upd PersonMP
    Upd TimetableMP
    Upd WorkoutMP
    Upd GymOM
    Add ClientForTimetableOM
    Add CoachForTimetableOM
    Add AddClientTimetableIM
    Add AddTimetableIM
    Add GetAllTimetablesWithCoachWorkoutsGymsClientsOM
    GetTimetablesOM
    Upd WorkoutOM
    Upd TimetableClient
    Add ConsoleApp4

---
+++ FitnessClub/FitnessClub.BLL/Models/TimetableModels/OutputModels/GetAllTimetablesWithCoachWorkoutsGymsClientsOutputModel.cs
@@ -0,0 +1,29 @@
+﻿using FitnessClub.BLL.Models.GymModels;
+using FitnessClub.BLL.Models.PersonModels.OutputModels;
+using FitnessClub.BLL.Models.SportTypeModels;
+using FitnessClub.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.BLL.Models.TimetableModels.OutputModels
+{
+    public class GetAllTimetablesWithCoachWorkoutsGymsClientsOutputModel
+    {
+        public int Id { get; set; }
+
+        public string DateTime { get; set; }
+
+        public List<ClientForTimetableOutputModel> Clients { get; set; }
+
+        public CoachForTimetableOutputModel Coach { get; set; }
+
+        public WorkoutRepository Workout { get; set; }
+
+        public GymOutputModel Gym { get; set; }
+
+        public SportTypeOutputModel SportType { get; set; }
+    }
+}
--- FitnessClub/FitnessClub.BLL/Models/WorkoutModels/OutputModels/WorkoutOutputModel.cs
+++ FitnessClub/FitnessClub.BLL/Models/WorkoutModels/OutputModels/WorkoutOutputModel.cs
@@ -8,6 +8,17 @@ namespace FitnessClub.BLL.Models.WorkoutModels.OutputModels
 {
     public class WorkoutOutputModel
     {
+        public int Id { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Duration { get; set; }
+
+        public int NumberPlaces { get; set; }
+
+        public bool IsGroup { get; set; }
+
+        public string? Comment { get; set; }
 
     }
 }
